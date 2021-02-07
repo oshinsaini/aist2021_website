2f847fbc19d8419d2d39b6457924ae0627046b44
Oshin Saini <oshinsaini@Oshins-MacBook-Pro.local>
2021-02-07 19:19:17 +0530

    fixed event-starts-in

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,22 +25,22 @@ $(document).ready(function() {
          
     }); 
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    })
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     nav:true,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:5
+    //         }
+    //     }
+    // })
     
 
     
@@ -68,79 +68,55 @@ $(document).ready(function() {
 	$('#navigation').on('hide.bs.collapse', function () {
 	   $('.header').removeClass('header-has-bg');
 	});
-    
-	
+    	
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("Oct 12, 2024").getTime();
+  var target_date = new Date("Oct 12, 2021").getTime();
      
-    // variables for time units
-    var days, hours, minutes, seconds;
-     
-    // get tag element
-    var countdown =  document.getElementById("countdown-box");
-    var days_span = document.createElement("SPAN");
-    days_span.className = 'days';
-    countdown.appendChild(days_span);
-    var hours_span = document.createElement("SPAN");
-    hours_span.className = 'hours';
-    countdown.appendChild(hours_span);
-    var minutes_span = document.createElement("SPAN");
-    minutes_span.className = 'minutes';
-    countdown.appendChild(minutes_span);
-    var secs_span = document.createElement("SPAN");
-    secs_span.className = 'secs';
-    countdown.appendChild(secs_span);
-     
-    // update the tag with id "countdown" every 1 second
-    setInterval(function () {
-     
-        // find the amount of "seconds" between now and target
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
-     
-        // do some time calculations
-        days = parseInt(seconds_left / 86400);
-        seconds_left = seconds_left % 86400;
-         
-        hours = parseInt(seconds_left / 3600);
-        seconds_left = seconds_left % 3600;
-         
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
-         
-        // format countdown string + set tag value.
-        days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
-        hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
-        minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
-        secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>'; 
-     
-    }, 1000);
-    
-
-
-
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      },
-      1200: {
-        items: 5
-      }
-    }
-  });
-        
+  // variables for time units
+  var days, hours, minutes, seconds;
+   
+  // get tag element
+  var countdown =  document.getElementById("countdown-box");
+  var days_span = document.createElement("SPAN");
+  days_span.className = 'days';
+  countdown.appendChild(days_span);
+  var hours_span = document.createElement("SPAN");
+  hours_span.className = 'hours';
+  countdown.appendChild(hours_span);
+  var minutes_span = document.createElement("SPAN");
+  minutes_span.className = 'minutes';
+  countdown.appendChild(minutes_span);
+  var secs_span = document.createElement("SPAN");
+  secs_span.className = 'secs';
+  countdown.appendChild(secs_span);
+   
+  // update the tag with id "countdown" every 1 second
+  setInterval(function () {
+   
+      // find the amount of "seconds" between now and target
+      var current_date = new Date().getTime();
+      var seconds_left = (target_date - current_date) / 1000;
+   
+      // do some time calculations
+      days = parseInt(seconds_left / 86400);
+      seconds_left = seconds_left % 86400;
+       
+      hours = parseInt(seconds_left / 3600);
+      seconds_left = seconds_left % 3600;
+       
+      minutes = parseInt(seconds_left / 60);
+      seconds = parseInt(seconds_left % 60);
+       
+      // format countdown string + set tag value.
+      days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
+      hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
+      minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
+      secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>'; 
+   
+  }, 1000);
+  
+  
+      
 
 });
