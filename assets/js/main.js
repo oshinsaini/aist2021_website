8c994776bea42bdb70f8e33ce8d21c14072706f0
Oshin Saini <oshinsaini@Oshins-MacBook-Pro.local>
2021-02-07 15:49:50 +0530

    videos.gallery.footer.map.etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,25 @@ $(document).ready(function() {
          
          
     }); 
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
     
+
     
     /* ======= Scrollspy ======= */
     $('body').scrollspy({ target: '#header', offset: 400});
@@ -99,7 +117,30 @@ $(document).ready(function() {
      
     }, 1000);
     
-    
+
+
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
         
 
 });
